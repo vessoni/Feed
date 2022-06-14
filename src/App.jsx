845171e4794e4ab0a6a8b45c3efd1ecc47da4d22
Alexandre Vessoni <av.vessoni@gmail.com>
2022-06-15 00:08:05 +0100

    Added globalStyles + header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Post } from './Post';
 
+
+import './global.css'
 function App() {
   return (
   <Post author='Alex' content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam corrupti, id, consequuntur iste sequi provident nesciunt, vitae laboriosam officiis omnis quo dolorem dolores? Quo quidem odio, id deleniti ullam sint!'/>
