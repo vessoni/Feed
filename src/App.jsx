2f564fcc217a4da214c167029067cff0ea4d4ef4
Alexandre Vessoni <av.vessoni@gmail.com>
2022-06-15 23:38:15 +0100

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { Post } from './Post';
+import { Header } from "./components/Header";
 
 
 import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
+
 function App() {
   return (
-  <Post author='Alex' content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam corrupti, id, consequuntur iste sequi provident nesciunt, vitae laboriosam officiis omnis quo dolorem dolores? Quo quidem odio, id deleniti ullam sint!'/>
+    <div>
+    <Header />
+    <div className={styles.wrapper}>
+
+      <Sidebar />
+      <main>
+
+      <Post author='Alex' content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam corrupti, id, consequuntur iste sequi provident nesciunt, vitae laboriosam officiis omnis quo dolorem dolores? Quo quidem odio, id deleniti ullam sint!'/>
+      <Post author='Alex' content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam corrupti, id, consequuntur iste sequi provident nesciunt, vitae laboriosam officiis omnis quo dolorem dolores? Quo quidem odio, id deleniti ullam sint!'/>
+      </main>
+
+   
+    </div>
+  
+    </div>
   )
 }
 
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,7 @@
-import { Header } from "./components/Header";
-
 export function Post(props){
   return (
     <div>
-      <Header />
+    
       <strong>{props.author}</strong>
       <p>{props.content}</p>
     </div>
