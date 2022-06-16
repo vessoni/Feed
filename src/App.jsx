19185946ea420a92a20763560494be4617a6134e
Alexandre Vessoni <vessonia@jbs.cam.ac.uk>
2022-06-16 11:15:38 +0100

    Post - styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Post } from './Post';
+import { Post } from './components/Post';
 import { Header } from "./components/Header";
+import { Sidebar } from './components/Sidebar';
 
 
 import './global.css'
 
 import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar';
+
 
 function App() {
   return (
