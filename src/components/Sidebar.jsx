21dfa7750e613c81e580a9a364962e47c522e0c2
Alexandre Vessoni <vessonia@jbs.cam.ac.uk>
2022-06-16 10:27:19 +0100

    Sidebar - CSS

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Siderbar.module.css';
 
 export function Sidebar(){
@@ -6,12 +7,15 @@ export function Sidebar(){
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
     
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/4910383?v=4" />
+
         <strong>Alexandre Vessoni</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="https://">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
